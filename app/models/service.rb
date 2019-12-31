0ae32e4a5a4a217c1ac432a54a746d7ae33084f3
吉野輝 <shoynoi.jp@gmail.com>
2019-12-31 12:28:53 +0900

    Serviceモデルにバリデーションを追加

--- app/models/service.rb
+++ app/models/service.rb
@@ -1,2 +1,7 @@
 class Service < ApplicationRecord
+  enum plan: { monthly: 0, yearly: 1 }
+
+  validates :name, presence: true
+  validates :plan, presence: true
+  validates :price, numericality: { only_integer: true, allow_blank: true }
 end
