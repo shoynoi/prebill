aa98207450440d38ecdf4d1152bec03f734c4441
吉野輝 <shoynoi.jp@gmail.com>
2019-12-31 12:28:24 +0900

    サービスを登録できる機能を追加

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -1,18 +1,48 @@
 # frozen_string_literal: true
 
 class ServicesController < ApplicationController
+  before_action :load_service, only: %i(edit update destroy)
+
   def new
+    @service = Service.new
   end
 
   def create
+    @service = Service.new(service_params)
+    if @service.save
+      redirect_to root_path, notice: "新しいサービスを登録しました。"
+    else
+      render :new
+    end
   end
 
   def edit
   end
 
   def update
+    if @service.update(service_params)
+      redirect_to root_path, notice: "サービスを修正しました。"
+    end
   end
 
   def destroy
+    if @service.destroy
+      redirect_to root_path, notice: "サービスを削除しました。"
+    end
   end
+
+  private
+    def service_params
+      params.require(:service).permit(
+        :name,
+        :description,
+        :plan,
+        :price,
+        :renewed_on,
+        :notified_on)
+    end
+
+    def load_service
+      @service = Service.find(params[:id])
+    end
 end
