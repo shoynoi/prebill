41bf06def03520a488a56c6f011d0b21764b5fae
吉野輝 <shoynoi.jp@gmail.com>
2019-12-31 10:15:22 +0900

    i18nの設定を追加

--- Gemfile
+++ Gemfile
@@ -18,6 +18,7 @@ gem "jbuilder", "~> 2.7"
 # gem 'bcrypt', '~> 3.1.7'
 gem "slim-rails"
 gem "html2slim"
+gem "rails-i18n", "~> 6.0.0"
 
 # Use Active Storage variant
 # gem 'image_processing', '~> 1.2'
--- config/application.rb
+++ config/application.rb
@@ -17,5 +17,7 @@ module Prebill
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    config.time_zone = "Tokyo"
+    config.i18n.default_locale = :ja
   end
 end
