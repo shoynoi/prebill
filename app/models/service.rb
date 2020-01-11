7daea7f1e99adfe21e0eb9b2053815e7d9a31c76
吉野輝 <shoynoi.jp@gmail.com>
2020-01-12 01:00:24 +0900

    サービスの料金について空欄を許可するように修正

--- app/models/service.rb
+++ app/models/service.rb
@@ -5,5 +5,5 @@ class Service < ApplicationRecord
 
   validates :name, presence: true
   validates :plan, presence: true
-  validates :price, numericality: { only_integer: true }
+  validates :price, numericality: { only_integer: true, allow_blank: true }
 end
