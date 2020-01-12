0b2d230b8475b3fcb0f7d8d14f3e241733f94e74
吉野輝 <shoynoi.jp@gmail.com>
2020-01-12 22:10:38 +0900

    ユーザーの登録機能を作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,24 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to root_path, notice: "ユーザー「#{@user.name}」を登録しました"
+    else
+      render :new
+    end
   end
+
+  private
+    def user_params
+      params.require(:user).permit(
+        :name,
+        :email,
+        :password,
+        :password_confirmation,
+        )
+    end
 end
