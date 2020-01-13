5de97590dcebc5cc5f73096681ec0ce7886cb863
吉野輝 <shoynoi.jp@gmail.com>
2020-01-13 10:34:52 +0900

    認証失敗時に入力された内容を保持するように修正

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -1,11 +1,17 @@
 class UserSessionsController < ApplicationController
+
+  def new
+    @user = User.new
+  end
+
   def create
     @user = login(params[:email], params[:password])
 
     if @user
       redirect_back_or_to root_path, notice: "ログインしました。"
     else
-      flash.now[:alert] = "ユーザー名かパスワードが正しくありません。"
+      @user = User.new(email: params[:user][:email], password: params[:user][:password])
+      flash.now[:alert] = "メールアドレスかパスワードが正しくありません。"
       render :new
     end
   end
