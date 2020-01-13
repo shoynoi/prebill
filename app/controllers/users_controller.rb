9b9f12f5bcc2e458c46f8ee730d9bde23c4d8370
吉野輝 <shoynoi.jp@gmail.com>
2020-01-13 19:07:21 +0900

    ユーザー情報変更ページを作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,9 @@
 class UsersController < ApplicationController
-  layout "welcome"
-  skip_before_action :require_login
+  skip_before_action :require_login, only: %i(new create)
 
   def new
     @user = User.new
+    render layout: "welcome"
   end
 
   def create
@@ -11,8 +11,25 @@ class UsersController < ApplicationController
     if @user.save
       redirect_to root_path, notice: "ユーザー「#{@user.name}」を登録しました"
     else
-      render :new
+      render :new, layout: "welcome"
     end
+  end
+
+  def edit
+    @user = current_user
+  end
+
+  def update
+    if current_user.update(user_params)
+      redirect_to root_path, notice: "ユーザー情報を変更しました。"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    current_user.destroy
+    redirect_to root_path, notice: "退会が完了しました。"
   end
 
   private
