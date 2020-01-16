55e2d93df5dbd249df396d7204ac354398b9bd97
吉野輝 <shoynoi.jp@gmail.com>
2020-01-16 15:01:55 +0900

    Userモデルのバリデーションを修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,8 @@ class User < ApplicationRecord
   authenticates_with_sorcery!
 
   validates :password, length: { minimum: 6 }, confirmation: true, if: :password_required?
-  validates :password_confirmation, presence: true, if: :password_required?
 
-  validates :email, uniqueness: true
+  validates :email, uniqueness: true, presence: true
   validates :name, presence: true
 
   private
