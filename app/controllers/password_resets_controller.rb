e02a8469e20e2710ec68e42286604eb3692ed6ce
吉野輝 <shoynoi.jp@gmail.com>
2020-01-16 18:07:36 +0900

    パスワードを再設定できる機能を追加

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -1,10 +1,42 @@
 class PasswordResetsController < ApplicationController
+  skip_before_action :require_login
+
+  def new
+  end
+
   def create
+    @user = User.find_by_email(params[:email])
+    if @user
+      @user.deliver_reset_password_instructions!
+      redirect_to login_path, notice: "パスワード再設定用のメールを送信しました。"
+    else
+      redirect_to login_path, alert: "メールアドレスが正しくありません。"
+    end
   end
 
   def edit
+    @token = params[:id]
+    @user = User.load_from_reset_password_token(params[:id])
+
+    if @user.blank?
+      not_authenticated
+    end
   end
 
   def update
+    @token = params[:id]
+    @user = User.load_from_reset_password_token(params[:id])
+
+    if @user.blank?
+      not_authenticated
+      return
+    end
+
+    @user.password_confirmation = params[:user][:password_confirmation]
+    if @user.change_password(params[:user][:password])
+      redirect_to(root_path, notice: "パスワードの再設定が完了しました。")
+    else
+      render :edit
+    end
   end
 end
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -6,9 +6,10 @@ class UserMailer < ApplicationMailer
   #
   #   en.user_mailer.reset_password_email.subject
   #
-  def reset_password_email
-    @greeting = "Hi"
-
-    mail to: "to@example.org"
+  def reset_password_email(user)
+    @user = User.find user.id
+    @url  = edit_password_reset_url(@user.reset_password_token)
+    mail(to: user.email,
+         subject: "PreBill パスワードのリセット")
   end
 end
