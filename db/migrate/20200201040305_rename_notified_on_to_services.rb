a80c7b2ba0ec93b91e93068f13c57d8230b262ef
吉野輝 <shoynoi.jp@gmail.com>
2020-02-02 16:22:58 +0900

    fin lint rubocop

--- db/migrate/20200201040305_rename_notified_on_to_services.rb
+++ db/migrate/20200201040305_rename_notified_on_to_services.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class RenameNotifiedOnToServices < ActiveRecord::Migration[6.0]
   def change
     rename_column :services, :notified_on, :remind_on
--- db/migrate/20200202053618_add_remind_sent_at_to_users.rb
+++ db/migrate/20200202053618_add_remind_sent_at_to_users.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class AddRemindSentAtToUsers < ActiveRecord::Migration[6.0]
   def change
     add_column :users, :remind_sent_at, :datetime
