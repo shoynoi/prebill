b865a5db56cf3ec6466f647638e8990acf1eb5cc
吉野輝 <shoynoi.jp@gmail.com>
2020-02-27 17:24:15 +0900

    notification.jsをapp/javascript/に移動

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,7 +6,7 @@
 require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
-require("./notification")
+require("../notification")
 require("../autocomplete")
 
 
