926fc5043c543a53edd1c234a845e5cb4a90f872
吉野輝 <shoynoi.jp@gmail.com>
2020-02-27 22:52:19 +0900

    アプリ内通知を読み込めていなかったのを修正

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,9 +6,8 @@
 require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
-require("../notification")
-require("../autocomplete")
-
+require("../notification.js")
+require("../autocomplete.js")
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
