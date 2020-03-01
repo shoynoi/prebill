278db8791cbabc9d27d68901fb079deb5652026c
吉野輝 <shoynoi.jp@gmail.com>
2020-03-01 09:41:13 +0900

    Flashの文言の修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to root_path, notice: "アカウントを作成しました。"
+      redirect_to root_path, notice: "アカウントを作成しました！"
     else
       render :new, layout: "welcome"
     end
