559b226c9fa14f3b469f2577b6c2a1e2715888ae
吉野輝 <shoynoi.jp@gmail.com>
2020-03-02 11:56:38 +0900

    本番環境に投入するCSVデータを作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,16 @@
 # frozen_string_literal: true
 
-Rake::Task["db:fixtures:load"].execute
+require "csv"
+
+case Rails.env
+when "production"
+  CSV.foreach("db/preset_services.csv", headers: true) do |row|
+    PresetService.find_or_create_by!(
+      name: row["name"],
+      plan: row["plan"],
+      price: row["price"]
+      )
+  end
+when "development"
+  Rake::Task["db:fixtures:load"].execute
+end
