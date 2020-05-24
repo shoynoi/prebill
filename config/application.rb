001e751d608f2c470b34b149fe0c40e99c511e01
吉野輝 <shoynoi.jp@gmail.com>
2020-05-24 11:04:45 +0900

    RSpecのジェネレータで自動生成するファイルの設定を変更

--- config/application.rb
+++ config/application.rb
@@ -32,5 +32,12 @@ module Prebill
     config.action_view.field_error_proc = Proc.new do |html_tag, instance|
       html_tag.html_safe
     end
+    config.generators do |g|
+      g.test_framework :rspec,
+                       fixtures: false,
+                       view_specs: false,
+                       helper_specs: false,
+                       routing_specs: false
+      end
   end
 end
